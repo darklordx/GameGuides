1576f4282e9f5052b16c4f4573efba4fe9b6bb63
darklordx <rowechen@gmail.com>
2022-04-05 15:17:19 -0500

    Initial commit

---
+++ GameGuides/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using GameGuides.ViewModels;
+using GameGuides.Views;
+using Xamarin.Forms;
+
+namespace GameGuides
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ GameGuides/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using GameGuides.ViewModels;
+
+namespace GameGuides.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
